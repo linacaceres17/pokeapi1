2bcbd627eba8ddf85711fe82c69d2dcae86fdda2
linacaceres17 <linchan17@gmail.com>
2022-12-09 15:30:13 -0500

    sexto

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -24,9 +24,9 @@ export class DetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getPokemon;
+    this.getPokemon();
   }
-  get getPokemon(){
+  public getPokemon(){
     const id = this.activatedRoute.snapshot.params['id']; 
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
